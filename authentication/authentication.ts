e0c0a4b3c42060633b2ecf76b5888be2518455fd
Joao <joaop86@hotmail.com>
2022-10-07 12:01:09 +0100

    get open PRs

--- authentication/authentication.ts
+++ authentication/authentication.ts
@@ -2,13 +2,16 @@ import axios from "axios";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const auth = axios.post("https://bitbucket.org/site/oauth2/access_token", {
-  body: "grant_type=urn:bitbucket:oauth2:jwt",
-  headers: {
-    Authorization: `JWT ${process.env.BITBUCKET_API_TOKEN}`,
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  method: "POST"
-});
+const auth = axios.post<{ access_token: string }>(
+  "https://bitbucket.org/site/oauth2/access_token",
+  { grant_type: "client_credentials" },
+  {
+    headers: {
+      Authorization: `Basic ${process.env.BITBUCKET_BASIC_AUTH_CREDENTIAL}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+  }
+);
 
 export default auth;
--- index.ts
+++ index.ts
@@ -1,30 +1,31 @@
-import { Bitbucket } from 'bitbucket';
+import { Bitbucket } from "bitbucket";
 import * as dotenv from "dotenv";
-import auth from './authentication/authentication';
+import auth from "./authentication/authentication";
 dotenv.config();
 
-auth.then(token => {
-    console.log(token);
+auth
+  .then((token) => {
     const clientOptions = {
-        auth: {
-          token: token.data ?? '',
-        },
-      };
-        
-      const bitbucket = new Bitbucket(clientOptions);
-      
-      bitbucket.pullrequests.list({
-          repo_slug: 'replai-platform',
-          workspace: 'replai',
-          state: 'OPEN',
+      auth: {
+        token: token.data.access_token ?? "",
+      },
+    };
+
+    const bitbucket = new Bitbucket(clientOptions);
+
+    bitbucket.pullrequests
+      .list({
+        repo_slug: "replai-platform",
+        workspace: "replai",
+        state: "OPEN",
       })
-      .then(response => {
-          console.log(response);
+      .then((response) => {
+        console.log(response.data.values);
       })
-      .catch(error => {
-          console.log(error);
+      .catch((error) => {
+        console.log(error);
       });
-})
-.catch(error => {
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
