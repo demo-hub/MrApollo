f913f068b24edcc6f4c90cdcbc6c49d76f2d6506
Joao <joaop86@hotmail.com>
2022-10-12 20:43:34 +0100

    do not send messages if no prs

--- src/index.ts
+++ src/index.ts
@@ -19,19 +19,21 @@ auth
 
     const prs = await notifyPRsOpen(bitbucket);
 
-    // Read Slack token from the environment variables
-    const slackToken = process.env.SLACK_TOKEN;
+    if (process.env.SLACK_TOKEN && prs.length > 0) {
+      // Read Slack token from the environment variables
+      const slackToken = process.env.SLACK_TOKEN;
 
-    // Initialize Slack client
-    const slack = new WebClient(slackToken);
+      // Initialize Slack client
+      const slack = new WebClient(slackToken);
 
-    const formattedPRLinks = prs.map((pr) => pr.links?.html?.href).join("\n");
+      const formattedPRLinks = prs.map((pr) => pr.links?.html?.href).join("\n");
 
-    await slack.chat.postMessage({
-      channel: process.env.SLACK_CHANNEL_ID ?? "",
-      text: `@${process.env.SLACK_USER_TO_TAG} there are some PRs waiting for your review: ${formattedPRLinks}`,
-      link_names: true,
-    });
+      await slack.chat.postMessage({
+        channel: process.env.SLACK_CHANNEL_ID ?? "",
+        text: `@${process.env.SLACK_USER_TO_TAG} there are some PRs waiting for your review: ${formattedPRLinks}`,
+        link_names: true,
+      });
+    }
   })
   .catch((error) => {
     throw new Error(error);
