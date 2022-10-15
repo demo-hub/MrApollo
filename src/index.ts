9587634445cbf6bc0a506f19cec3bf55cf987711
Joao <joaop86@hotmail.com>
2022-10-15 18:12:56 +0100

    jira integration

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,58 @@
 import { WebClient } from "@slack/web-api";
 import { Bitbucket } from "bitbucket";
 import * as dotenv from "dotenv";
+import { Version3Client } from "jira.js";
 import auth from "./authentication";
 import { notifyPRsOpen, notifyPRsWithConflicts } from "./notifications";
 
 // load env variables
 dotenv.config();
 
+const MS_IN_A_DAY = 86400000;
+
 setInterval(
   async () => {
     const currentDate = new Date();
+
+    const client = new Version3Client({
+      host: process.env.JIRA_HOST ?? "",
+      authentication: {
+        basic: {
+          email: process.env.JIRA_EMAIL ?? "",
+          apiToken: process.env.JIRA_API_TOKEN ?? "",
+        },
+      },
+    });
+
+    const jiraIssues = await client.issueSearch.searchForIssuesUsingJql({
+      jql: `project = ${process.env.JIRA_TARGET_PROJECT} AND issuetype in (Story, Task) AND status = "Backlog"`,
+    });
+
+    for (const issue of jiraIssues.issues ?? []) {
+      const issueDetails = await client.issues.getIssue({
+        issueIdOrKey: issue.key,
+      });
+
+      const issueUpdatedDate = new Date(issueDetails.fields.updated);
+
+      const dateDiffInMs = currentDate.getTime() - issueUpdatedDate.getTime();
+
+      const dateDiffInDays = dateDiffInMs / MS_IN_A_DAY;
+
+      if (
+        dateDiffInDays >
+        parseInt(process.env.TIME_TO_NOTIFY_ISSUE_IN_DAYS ?? "30")
+      ) {
+        await client.issueComments.addComment({
+          issueIdOrKey: issue.key,
+          body: {
+            type: "text",
+            text: `This issue has been in the backlog for more than ${process.env.TIME_TO_NOTIFY_ISSUE_IN_DAYS} days without updates. Please review if it still makes sense to have it.`,
+            version: 1,
+          },
+        });
+      }
+    }
 
     if (
       currentDate.getHours() >=
@@ -51,5 +94,5 @@ setInterval(
   },
   process.env.MESSAGES_INTERVAL_IN_MS
     ? parseInt(process.env.MESSAGES_INTERVAL_IN_MS)
-    : 10000 // 10 seconds
+    : 100 // 10 seconds
 );
